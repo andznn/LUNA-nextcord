068daa1fd57e96979a7701e13c5fd909f1e1ecf7
andznn <andrzejgawlowski.q@gmail.com>
2023-06-10 12:09:20 +0200

    Put bot in a function and added main.py to run it

--- main.py
+++ main.py
@@ -2,33 +2,6 @@
 """
 This is a main file of LUNA✱ Discord Bot Created by Andrew
 """
-# ----------------------------------------------------- Libraries ------------------------------------------------------
-import nextcord
-from nextcord.ext import *
-from nextcord import *
-import json
-import random
-import datetime
-import colorama
-from colorama import Fore, Style
-import sys
-from bot_token import token
-from reddit_setup import *
-from config.colors import lunaorange, lunablue
-from api_keys import *
-import aiohttp
-import asyncio
-from config.config import *
-import requests
-import dateutils
-import os
-import wikipedia
-import time
-import gtts
-from gtts import gTTS
-import logging
-from bot import luna_bot
-from time import time
-
 # --------------------------------------------- Running the main bot file ----------------------------------------------
+from bot import luna_bot
 luna_bot()
